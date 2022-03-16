425de601b79419ffc6fe79a13ba3bdbb5aef2c0e
LauraAzuaje <lyam35.25@gmail.com>
2022-03-16 10:42:32 -0500

    registerValidation

--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,13 +1,39 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-
 import { PETITIONS } from '../../../requestUrl'
+import './register.css'
+  
 
 const Register = () => {
   const [user, setUser] = useState({})
+  const [errors, setErrors] = useState({})
+
+  const validate = values => {
+    const error = {}
+    if (!values.name) {
+      error.name = 'Required';
+    } else if (values.name.test > 15) {
+      error.name = 'Este campo es obligatorio';
+    }
+  
+    if (!values.lastName) {
+      error.lastName = 'Required';
+    } else if (values.lastName.test > 20) {
+      error.lastName = 'Este campo es obligatorio';
+    }
+  
+    if (!values.email) {
+      error.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      error.email = "Invalid email address";
+    }
+  
+    return error;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setErrors(validate(user))
 
     axios.post(PETITIONS.register, user )
     .then(res => {
@@ -22,6 +48,12 @@ const Register = () => {
 
   }
 
+  const handleLastName = (e) => {
+    const lastName = e.target.value
+    setUser({...user, lastName})
+
+  }
+
   const handleEmail = (e) => {
     const email = e.target.value
     setUser({...user, email})
@@ -32,20 +64,27 @@ const Register = () => {
     setUser({...user, password})
   }
 
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label>Name</label>
+    <div className='container'>
+      <form className='form' onSubmit={handleSubmit}>
+      <h1> Registrate </h1>
+        <label>Nombre</label>
         <input type='text' onChange={handleName}/>
-
-        <label>Email</label>
+        {errors.name && <p>{errors.name}</p>}
+        <label>Apellido</label>
+        <input type='text' onChange={handleLastName}/>
+        {errors.lastName && <p>{errors.lastName}</p>}
+        <label>Correo</label>
         <input type='email'onChange={handleEmail}/>
 
-        <label>Password</label>
+        <label>Contraseña</label>
         <input type='password'onChange={handlePassword}/>
 
-        <input type='submit'/>
+        <input className='buttonS' type='submit'/>
       </form>
+      </div>
     </>
   )
 }
