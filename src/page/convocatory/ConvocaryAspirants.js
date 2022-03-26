168ba48b60bf31c4245695069eaaa34d61ae89e0
Edez84 <edhernandez84@misena.edu.co>
2022-03-25 23:15:42 -0500

    AspirantsInConvocatory

--- src/page/convocatory/ConvocaryAspirants.js
+++ src/page/convocatory/ConvocaryAspirants.js
@@ -1,47 +1,63 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import convocatorycss from "./Convocatory.module.css";
 
 const ConvocatoryAspirants = () => {
+  const [convAsp, setConvAsp] = useState([]);
 
-    const [convocatoryAspirants, setConvocatoryAspirants] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get(
+        "http://localhost:3001/api/admin/convocatories"
+      );
+      setConvAsp(data);
+    }
+    fetchData();
+  }, []);
+  console.log(convAsp);
 
-    useEffect(() => {
-        try {
-          axios.get('http://localhost:3001/api/admin/convocatories', {
-          }).then(res => {
-            setConvocatoryAspirants(res.data)
-            console.log (res.data)
-          })
-        } catch (error) {
-          console.log (error)
+  const [apirantsConvocatory, setApirantsConvocatory] = useState([]);
+  const token = useSelector((state) => state.token);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get(
+        "http://localhost:3001/api/user/all_info/:page",
+        {
+          headers: { Authorization: token },
         }
-      }, 
-      [setConvocatoryAspirants])
-    
-      const [apirantsConvocatory, setApirantsConvocatory] = useState([]);
+      );
 
-      useEffect(() => {
-          try {
-            axios.get('', {
-            }).then(res => {
-              setApirantsConvocatory(res.info)
-              console.log (res.info)
-            })
-          } catch (error) {
-            console.log (error)
-          }
-        }, 
-        [setApirantsConvocatory])
+      setApirantsConvocatory(data.users);
+    }
+    fetchData();
+  }, []);
+  console.log(apirantsConvocatory);
 
-return(
+  return (
     <>
-        <h1>hola</h1>
-          
+      {convAsp?.map((item) => (
+        <div className="section__convocatory">
+          <h1 key={item._id}>{item.name}</h1>
+            {" "}
+            {item.usersRegistered.map((i) =>
+              apirantsConvocatory?.map((aspirant) =>
+                i === aspirant._id ? <p>{aspirant.names}</p> : ""
+              )
+            )}
+        </div>
+      ))}
+      ;
     </>
-        ); 
-    
-    };
-    
+  );
+};
 
 export default ConvocatoryAspirants;
