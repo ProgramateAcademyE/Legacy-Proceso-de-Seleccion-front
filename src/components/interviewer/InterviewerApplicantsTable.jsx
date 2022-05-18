bc9dbb0b05da40dea14c4ecad9894c25d1c4faec
DIANACH <dianepatrich@gmail.com>
2022-05-18 09:41:46 -0500

    datatable

--- src/components/interviewer/InterviewerApplicantsTable.jsx
+++ src/components/interviewer/InterviewerApplicantsTable.jsx
@@ -1,72 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import DataTable, {createTheme} from 'react-data-table-component';
+import React, { useEffect, useState } from "react";
+import DataTable, { createTheme } from "react-data-table-component";
 import "styled-components";
 import "./InterviewerApplicants.css";
-import SortIcon from "@material-ui/icons/ArrowDownward";
-
-
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
 
 const InterviewerApplicantsTable = () => {
+  //1 - Configurar los hooks
+  const [users, setUsers] = useState([]);
+
+  //2 - Función para mostrar los datos con fetch
+  const URL = "http://localhost:3001/entrevistados";
+  const showData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    showData();
+  }, []);
+
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+    },
+    {
+      name: "FECHA",
+      selector: (row) => row.date,
+    },
+    {
+      name: "HORARIO",
+      selector: (row) => row.hour,
+    },
+    {
+      name: "ENTREVISTADOR",
+      selector: (row) => row.interviewer,
+    },
+    {
+      name: "OBSERVADOR",
+      selector: (row) => row.viewer,
+    },
+    {
+      name: "IDENTIFICACION",
+      selector: (row) => row.identification,
+    },
+    {
+      name: "ASPIRANTE",
+      selector: (row) => row.name,
+    },
   
-
-    //configurar hooks
-    const [users, setUsers] =useState([])
-
-    //fetch API
-    const URL = "http://localhost:3001/entrevistados"
-    const showData = async ()=>{
-     const response = await fetch(URL)
-     const data     = await response.json()
-     console.log (data)
-     setUsers(data)
-    }
-
-    useEffect( ()=>{
-        showData()
-    },[])
-
-    //configurar tabla
-    const columns =[
-        {
-            name: "ID",
-            selector: (row) => row.id,
-            sortable: true,
-            reorder: true
-        },
-        {
-            name: "NAME",
-            selector: (row) => row.name,
-            sortable: true,
-            reorder: true
-        },
-        {
-            name: "EMAIL",
-            selector: (row) => row.email,
-            sortable: true,
-            reorder: true
-        },
-    ]
-
+  ];
 
   return (
-    <div>   
-      INTERVIEWER TABLE
-
-      <DataTable 
-      selectableRows 
-      columns={columns}
-      data={users}
-      pagination
-      paginationRowsPerPageOptions={[5, 10, 25, 50, 100]}
-      responsive
-      defaultSortFieldId={1}
-      defaultSortAsc={false}
-      sortIcon={<SortIcon />}          
-      selectableRowsHighlight
-      />
-
+    <div className="main">
+      <DataTableExtensions
+        columns={columns}
+         data={users}
+         >
+        <DataTable
+          title = "Aspirantes Citados"
+          columns={columns}
+          data={users}
+          defaultSortField="id"
+          defaultSortAsc={false}
+          pagination
+          paginationRowsPerPageOptions={[5, 10, 25, 50, 100]}
+          highlightOnHover
+          selectableRows
+          selectableRowsHighlight
+          fixedHeader
+          fixedHeaderScrollHeight="400px"
+          responsive
+        />
+        </DataTableExtensions>
+   
     </div>
-  )
-}
-
-export default InterviewerApplicantsTable
+  );
+};
+export default InterviewerApplicantsTable;
