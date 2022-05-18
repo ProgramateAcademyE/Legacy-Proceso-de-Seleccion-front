71661190d1ea6dbe61ac01f7edd86d3fd68e0659
carolo180 <caponeris1@gmail.com>
2022-05-18 12:26:51 -0500

    upload testing front conection with backend

--- src/page/Moderator/ModeratorInterviewer_Viewer/ModeratorViewer.jsx
+++ src/page/Moderator/ModeratorInterviewer_Viewer/ModeratorViewer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import "./ModeratorInterviewer_Viewer.css";
 
 const ModeratorViewer = () => {
   const [users, setUsers] = useState([]);
-  const [citationUsers, setUsersSelected] = useState([]);
+
   const token = useSelector((state) => state.token);
 
   async function fetchUser() {
@@ -15,10 +17,10 @@ const ModeratorViewer = () => {
       }
       
     );
-    console.log(data)
+    
     setUsers(data);
     
-    setUsersSelected(data?.data[0]);
+   
   }
 
   useEffect(() => {
@@ -26,13 +28,30 @@ const ModeratorViewer = () => {
     
   }, []);
 
- 
+  console.log("users:", users)
 
   return (
     <>
     <div className="moderatorContainer">
         <div className="moderatorInterviewerContainer">
      <h1 className="moderatorInterviewerTitle">MODERADOR - OBSERVADOR</h1>
+     <table>
+        <tr>
+          <th>Staff</th>
+          <th>Assign rol</th>
+        </tr>
+       {
+         users.map((staff) => (
+          <tr>
+          <td>{staff.names}</td>
+          <td>{staff.role}</td>
+          </tr>
+          
+         )
+          
+         )
+       }
+     </table>
      </div>
      </div>
     </>
