9b498dd1b37e09c41c47aaa17ee883bc7c68c39e
JocelynRincon <jocelyn_vanessa@hotmail.com>
2022-05-18 10:54:56 -0500

    viewer

--- src/components/createInterview_Viewer/CreateViewer.jsx
+++ src/components/createInterview_Viewer/CreateViewer.jsx
@@ -1,44 +1,81 @@
-import React, { Component } from 'react'
-// import ReactDOM from "react-dom";
-import DataTable from 'react-data-table-component';
-import DataTableExtensions from "react-data-table-component-extensions";
+import React from 'react'
 import "./CreateInterviewer.css";
+import DataTable from 'react-data-table-component';
  
 
 
-function CreateViewer() {
+export const datacreateviewer = [
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
+  {observador:'Juan Fernando', date: "4/05/2022", jornada: 'Am', habilitar: 'x'},
 
+];
 
-
-const paginacionOpciones={
-    rowsPerPageText: "Filas por Página",
-    rangeSeparatorText: "de",
-    selectAllRowsItem: true,
-    selectAllRowsItemText: "Todos"
-}
-
-
-   
+const columnscreateviewer = [
+  {
+      name:"Observador",
+      selector:"observador",
+      sortable:true
+  },
+  {
+      name:"Fecha disponible",
+      selector:"date",
+      type: "numeric",
+      sortable: true
+  },
+  {
+      name:"Jornada disponible",
+      selector:"jornada",
+      sortable: true,
+      center: true
+  },
+  {
+      name:"Habilitar",
+      selector:"habilitar",
+      sortable: true,
+      center: true
   
-    return (
-      <div className="main">
-        <DataTableExtensions {...tableData}>
-          <DataTable
-            columns={columnscreateviewer}
-            data={datacreateviewer}
-            noHeader
-            defaultSortField="id"
-            defaultSortAsc={false}
-            pagination
-            highlightOnHover
-          />
-        </DataTableExtensions>
-      </div>
-    );
   }
-  
-//   const rootElement = document.getElementById("root");
-//   ReactDOM.render(<CreateViewer />, rootElement);
+      ];
+
+      const paginacionOpciones={
+        rowsPerPageText: "Filas por Página",
+        rangeSeparatorText: "de",
+        selectAllRowsItem: true,
+        selectAllRowsItemText: "Todos"
+    }
+
+
+function CreateViewer() {
+         return (
+        <div>
+    <DataTable 
+    columns={columnscreateviewer}
+    data={datacreateviewer}
+    title="Observadores"
+    pagination
+    paginationComponentOptions={paginacionOpciones}
+    fixedHeader
+    fixedHeaderScrollHeight='500px'
+    /> 
+    
+     </div>
+
+  );
+}
 
  export default CreateViewer
 
