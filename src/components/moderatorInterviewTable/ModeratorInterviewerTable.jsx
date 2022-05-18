698f93f8efff9301304b42e9ce0ebb7400eaa5ec
doraly <doraly9110@hotmail.com>
2022-05-18 15:13:29 -0500

    agregando datos desde api

--- src/components/moderatorInterviewTable/ModeratorInterviewerTable.jsx
+++ src/components/moderatorInterviewTable/ModeratorInterviewerTable.jsx
@@ -1,29 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
+import "styled-components";
 import DataTableExtensions from "react-data-table-component-extensions";
 import "react-data-table-component-extensions/dist/index.css";
-import { columns, data } from "./data";
+//import { columns, data } from "./data";
 
-function ModeratorInterviewerTable() {
-  const tableData = {
+
+const ModeratorInterviewerTable= ()=> {
+   //1 - Configurar los hooks
+  const [users, setUsers] = useState([])
+    //2 - Función para mostrar los datos con fetch
+  const URL= "http://localhost:3002/interviewTable";
+  const showData = async() =>{
+    const response = await fetch(URL)
+    const data = await response.json()
+    console.log(data)
+    setUsers(data)
+
+  }
+  useEffect(()=>{
+    showData()
+
+  },[])
+//3 
+  const columns = [
+   
+    {
+      name: "FECHA",
+      selector: (row) => row.fecha,
+    },
+    {
+      name: "JORNADA",
+      selector: (row) => row.jornada,
+    },
+    {
+      name: "COnvocatoria",
+      selector: (row) => row.convocatoria,
+    },
+    {
+      name: "# ASPIRANTES",
+      selector: (row) => row.aspirante,
+    },
+    {
+      name: "# ENTREVISTADORES",
+      selector: (row) => row.entrevistador,
+    },
+    {
+      name: "# OBSERVADORES",
+      selector: (row) => row.observador,
+    },
+    {
+      name: "# SALAS",
+      selector: (row) => row.salas,
+    },
+    {
+      name: "DETALLE",
+      selector: (row) => row.detalle,
+    },
+  
+  ];
+
+
+  /*const tableData = {
     columns,
     data
-  };
+  };*/
 
   return (
     <div className="main">
-      <DataTableExtensions {...tableData}>
+      <DataTableExtensions 
+        columns={columns}
+        data={users}
+      >
         <DataTable
           columns={columns}
-          data={data}
+          data={users}
+          defaultSortField="id"
+          defaultSortAsc={false}
           //scroll
           fixedHeader
           fixedHeaderScrollHeight="500px"
           noHeader
-          //defaultSortField="id"
           pagination
           highlightOnHover
-          selectableRows
+          
         />
       </DataTableExtensions>
     </div>
