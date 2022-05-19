b6ced6c9f6cec3fe7ff2ac5e6acf4312ff9767b1
leandro <leaguts21@gmail.com>
2022-05-18 22:07:56 -0500

    intento noche

--- src/page/Viewer/ViewerDashboard.jsx
+++ src/page/Viewer/ViewerDashboard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./ViewerDashboard.css";
+import DataTableExtensions from "react-data-table-component-extensions";
+import "react-data-table-component-extensions/dist/index.css";
+
 
 const ViewerDashboard = () => {
   return (
     <>
     <div className="viewerDashboardContainer">
      <h1 className="viewrDashboardTitle">Inicio OBSERVADOR</h1>
+     
      </div>
     </>
   )
