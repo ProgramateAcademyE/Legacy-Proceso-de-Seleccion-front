37b19a802f5ff0cf13487e3616306dd0c49ff6b6
leandro <leaguts21@gmail.com>
2022-05-19 09:59:19 -0500

    tabla creada

--- src/page/Viewer/ViewerDashboard.jsx
+++ src/page/Viewer/ViewerDashboard.jsx
@@ -2,17 +2,84 @@ import React from 'react'
 import "./ViewerDashboard.css";
 import DataTableExtensions from "react-data-table-component-extensions";
 import "react-data-table-component-extensions/dist/index.css";
+import { useState,useEffect } from 'react';
+import DataTable from "react-data-table-component";
 
 
 const ViewerDashboard = () => {
+  //1 - Configurar los hooks
+  const [users, setUsers] = useState([]);
+
+  //2 - Función para mostrar los datos con fetch
+  const URL = "http://localhost:3002/entrevistados";
+  const showData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    showData();
+  }, []);
+
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+    },
+    {
+      name: "FECHA",
+      selector: (row) => row.date,
+    },
+    {
+      name: "HORARIO",
+      selector: (row) => row.hour,
+    },
+    {
+      name: "ASPIRANTE",
+      selector: (row) => row.name,
+    },
+    {
+      name: "IDENTIFICACION",
+      selector: (row) => row.identification,
+    },
+    {
+      name: "ENTREVISTADOR",
+      selector: (row) => row.interviewername,
+    },
+    {
+      name: "OBSERVADOR",
+      selector: (row) => row.viewername,
+    },  
+  
+  ];
+
   return (
-    <>
-    <div className="viewerDashboardContainer">
-     <h1 className="viewrDashboardTitle">Inicio OBSERVADOR</h1>
-     
-     </div>
-    </>
-  )
-}
+    <div className="table">
+      <DataTableExtensions
+        columns={columns}
+         data={users}
+         >
+        <DataTable
+          title = "Aspirantes Citados"
+          columns={columns}
+          data={users}
+          defaultSortField="id"
+          defaultSortAsc={false}
+          pagination
+          paginationRowsPerPageOptions={[5, 10, 25, 50, 100]}
+          highlightOnHover
+          selectableRows
+          selectableRowsHighlight
+          fixedHeader
+          fixedHeaderScrollHeight
+          
+        />
+      </DataTableExtensions>
+   
+    </div>
+  );
+};
 
 export default ViewerDashboard
