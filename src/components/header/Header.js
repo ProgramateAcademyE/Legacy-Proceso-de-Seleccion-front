444b7c02c52a22d5aeedb82ed86dd3afc7321606
DIANACH <dianepatrich@gmail.com>
2022-05-22 14:04:41 -0500

    sass updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -60,6 +60,7 @@ const Header = () => {
   return (
     <div className="General_header">
       <div className="header__logo" id="logo">
+        
         {auth ? (
           <Link to="/dashboard">
             <img
@@ -73,7 +74,8 @@ const Header = () => {
           <i className="fas fa-bars pointer" onClick={moveNav} />
         </div>
       </div>
-
+      <button>programate academy</button>
+      <button>programate school</button>
       <div className="header__user">
         <User />
       </div>
