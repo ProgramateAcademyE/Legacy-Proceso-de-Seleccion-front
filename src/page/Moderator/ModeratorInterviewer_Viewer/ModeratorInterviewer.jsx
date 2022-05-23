7a28abef97ab081e11b6ba8d60e73a133dcd3732
JocelynRincon <jocelyn_vanessa@hotmail.com>
2022-05-23 14:46:19 -0500

    responsive

--- src/page/Moderator/ModeratorInterviewer_Viewer/ModeratorInterviewer.jsx
+++ src/page/Moderator/ModeratorInterviewer_Viewer/ModeratorInterviewer.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import "./ModeratorInterviewer_Viewer.css";
 import CreateInterviewer from '../../../components/createInterview_Viewer/CreateInterviewer.jsx';
 import ModalCreateNewInterviewer from '../../../components/modals/ModalCreateNewInterviewer';
+import SelectButton from '../../../components/selectButton/SelectButton';
 
 const ModeratorInterviewer = () => {
   return (
     <>
     <div className="moderatorContainer_interviewer">
         <div className="moderatorInterviewerContainer">
-     {/* <h1 className="moderatorInterviewerTitle">MODERADOR - ENTREVISTADOR </h1> */}
+     <h1 className="moderatorInterviewerTitle">TABLA DE ENTREVISTADORES </h1> 
+     <SelectButton/>
      <ModalCreateNewInterviewer/>
+     
      <CreateInterviewer/>
      
      
