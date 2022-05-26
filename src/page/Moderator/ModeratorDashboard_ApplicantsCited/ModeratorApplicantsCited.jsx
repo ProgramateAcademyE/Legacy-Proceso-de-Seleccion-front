52c2cbce92425e1b7edf058061a5e6f640830896
DIANACH <dianepatrich@gmail.com>
2022-05-26 12:05:00 -0500

    select button

--- src/page/Moderator/ModeratorDashboard_ApplicantsCited/ModeratorApplicantsCited.jsx
+++ src/page/Moderator/ModeratorDashboard_ApplicantsCited/ModeratorApplicantsCited.jsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React from 'react'
 import "./ModeratorDashboard_ApplicantsCited.css";
 import DataTableExtensions from "react-data-table-component-extensions";
 import "react-data-table-component-extensions/dist/index.css";
-import { useState, useEffect } from "react";
-import DataTable from "react-data-table-component";
+import { useState,useEffect } from 'react';
+import DataTable  from "react-data-table-component";
 import "styled-components";
-import SelectButton from "../../../components/selectButton/SelectButton";
-import axios from "axios";
+import SelectButton from '../../../components/selectButton/SelectButton';
+import axios from 'axios';
 import { useSelector } from "react-redux";
+
 
 const ModeratorApplicantsCited = () => {
   const [users, setUsers] = useState([]);
 
-  /*   const URL = "http://localhost:3002/entrevistados";
+/*   const URL = "http://localhost:3002/entrevistados";
   const showData = async () => {
     const response = await fetch(URL);
     const data = await response.json();
@@ -20,22 +21,24 @@ const ModeratorApplicantsCited = () => {
     setUsers(data);
   }; */
 
-  const token = useSelector((state) => state.token);
+  // const token = useSelector((state) => state.token);
   async function showData() {
     const { data } = await axios.get(
-      "http://localhost:3001/api/admin/citation-all",
-      {
-        headers: { Authorization: token },
-      }
-    );
+    "http://localhost:3002/entrevistados",
+  /* {
+    headers: { Authorization: token },
+    }*/
+  );
     setUsers(data);
   }
 
+  
   useEffect(() => {
     showData();
   }, []);
 
   const columns = [
+   
     {
       name: "FECHA",
       selector: (row) => row.date,
@@ -55,45 +58,47 @@ const ModeratorApplicantsCited = () => {
     {
       name: "ID ADPIRANTE",
       selector: (row) => row.interviewername,
-    },
+    }
+  
   ];
 
+
+
   return (
-    //1 - Configurar los hooks
+      //1 - Configurar los hooks
 
     <div className="moderatorContainer43">
-      <h2>Aspirantes </h2>
-      <div>
-        <h4 className="">Por favor seleccione fecha y hora</h4>
-        <select className="selectButton">
-          <option value="">30/06/2022 AM</option>
-          <option value="">30/06/2022 PM</option>
-          <option value="">01/07/2022 AM</option>
-          <option value="">01/07/2022 PM</option>
-        </select>
-      </div>
-      <div className="moderatorApplicantsCitedContainer">
-        <div className="table">
-          <DataTableExtensions columns={columns} data={users}>
-            <DataTable
-              title="Aspirantes Citados"
-              columns={columns}
-              data={users}
-              defaultSortField="id"
-              defaultSortAsc={false}
-              pagination
-              paginationRowsPerPageOptions={[5, 10, 25, 50, 100]}
-              highlightOnHover
-              selectableRows
-              selectableRowsHighlight
-              fixedHeader
-              fixedHeaderScrollHeight
-            />
-          </DataTableExtensions>
-        </div>
-      </div>
+              <h2>Aspirantes </h2>
+              <SelectButton/>
+              <div className="moderatorApplicantsCitedContainer">
+                  <div className="table">
+                      <DataTableExtensions
+                        columns={columns}
+                        data={users}
+                        >
+                        <DataTable
+                          title = "Aspirantes Citados"
+                          columns={columns}
+                          data={users}
+                          defaultSortField="id"
+                          defaultSortAsc={false}
+                          pagination
+                          paginationRowsPerPageOptions={[5, 10, 25, 50, 100]}
+                          highlightOnHover
+                          selectableRows
+                          selectableRowsHighlight
+                          fixedHeader
+                          fixedHeaderScrollHeight
+                          
+                        />
+                      </DataTableExtensions>
+      
+                    </div>
+                </div>
+          
     </div>
-  );
-};
+    
+  )
+}
 
-export default ModeratorApplicantsCited;
+export default ModeratorApplicantsCited
