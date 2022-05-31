eeff60e33c163e347dcac53e2626e0056707ea92
DIANACH <dianepatrich@gmail.com>
2022-05-31 15:01:22 -0500

    viewer and interviewer cards

--- src/page/Invertiewer/InterviewerApplicantsCited.jsx
+++ src/page/Invertiewer/InterviewerApplicantsCited.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import InterviewerApplicantsCalificationBox from '../../components/interviewer/InterviewerApplicantsCalificationBox';
-import InterviewerApplicantsTable from '../../components/interviewer/InterviewerApplicantsTable';
+import React from "react";
+import InterviewerApplicantsCalificationBox from "../../components/interviewer/InterviewerApplicantsCalificationBox";
+import InterviewerApplicantsTable from "../../components/interviewer/InterviewerApplicantsTable";
 import "./InterviewerApplicant.css";
 
 const InterviewerApplicantsCited = () => {
   return (
     <>
       <div className="interviewerApplicantContainer">
-      <h1 className="interviewrApplicantTitle"><span className='interviewerSpan'>Aplicantes citados </span></h1>
-       <div className="">
+        <h1 className="interviewrApplicantTitle">
+          <span className="interviewerSpan">Aplicantes citados </span>
+        </h1>
+        <div className="">
+          <h4 className="viewerAssesmentTitle2">
+            Lista de estudiantes agendados para hoy...
+          </h4>
+          <InterviewerApplicantsTable />
 
-        <h4 className="viewerAssesmentTitle2">
-          Lista de estudiantes agendados para hoy...
-        </h4>
-       <InterviewerApplicantsTable/>
-
-       <h1 className="interviewrApplicantTitle2"><span className='interviewerSpan'>Aspirante</span></h1>
-       <InterviewerApplicantsCalificationBox />
-
-       </div>
-       </div>
+          <h1 className="interviewrApplicantTitle2">
+            <span className="interviewerSpan">Aspirante</span>
+          </h1>
+          <InterviewerApplicantsCalificationBox />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default InterviewerApplicantsCited
+export default InterviewerApplicantsCited;
